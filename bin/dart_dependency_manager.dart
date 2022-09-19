9cfa93043776f99a942262963cec1f639fb43635
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-09-19 17:25:35 +0800

    initial commit

---
+++ bin/dart_dependency_manager.dart
@@ -0,0 +1,5 @@
+import 'package:dart_dependency_manager/node.dart';
+
+void main(List<String> arguments) {
+  traverse(allDependencies.first);
+}
---
+++ test/dart_dependency_manager_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_dependency_manager/dart_dependency_manager.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
